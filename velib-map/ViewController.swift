568554b977035e7f539b46620902d88bf0837f85
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2017-04-16 14:53:35 +0200

    feat(master): initial commit

---
+++ velib-map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  velib-map
+//
+//  Created by Thomas Guilleminot on 16/04/2017.
+//  Copyright © 2017 Thomas Guilleminot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
