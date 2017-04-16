14fee8e05a303248ede6cb1035e8da4ae068b823
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2017-04-16 15:16:51 +0200

    feat(develop): add Just and SwiftyJSON

---
+++ velib-map/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  velib-map
+//
+//  Created by Thomas Guilleminot on 16/04/2017.
+//  Copyright © 2017 Thomas Guilleminot. All rights reserved.
+//
+
+import UIKit
+import Just
+import SwiftyJSON
+
+class MapViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+}
