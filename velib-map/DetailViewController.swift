6c6d8540b6ad06678a12d9dfe0b54b3d7ada5a8e
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2017-05-23 17:29:47 +0200

    feat(detail): show map on detail page

--- velib-map/DetailViewController.swift
+++ velib-map/DetailViewController.swift
@@ -7,17 +7,65 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class DetailViewController: UIViewController {
+  
+  @IBOutlet weak var mapView: MKMapView!
   
   var currentStation: Station?
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    let screenHeight = UIScreen.main.bounds.height
+    self.mapView.frame.size.height = screenHeight / 2
+    self.mapView.delegate = self
+    
+    self.navigationController?.navigationBar.tintColor = UIColor.white
     
     if let station = self.currentStation {
-      print("station number = \(station.number ?? -1)")
+      let coordinates = CLLocation(latitude: station.lat, longitude: station.lng)
+      self.title = station.title ?? "N/A"
+      self.mapView.addAnnotation(station)
+      self.centerMapOnLocation(location: coordinates)
     }
+    
+  }
+  
+  func centerMapOnLocation(location: CLLocation) {
+    let regionRadius: CLLocationDistance = 1000
+    let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                              regionRadius * 2.0, regionRadius * 2.0)
+    self.mapView.setRegion(coordinateRegion, animated: true)
   }
   
 }
+
+extension DetailViewController: MKMapViewDelegate {
+  func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+    guard let annotation = annotation as? Station
+      else { return nil }
+    
+    let identifier = "velibPin"
+    let pin: MKAnnotationView
+    var imageName = "pin"
+    
+    if annotation.status! == "CLOSED" {
+      imageName = "pin-red"
+    }
+    
+    if let deqeuedView = self.mapView.dequeueReusableAnnotationView(withIdentifier: identifier) {
+      deqeuedView.annotation = annotation
+      pin = deqeuedView
+    } else {
+      pin = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+    }
+    
+    pin.image = UIImage(named: imageName)
+    
+    return pin
+  }
+  
+}
+
