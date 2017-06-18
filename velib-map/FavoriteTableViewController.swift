26437d28fe3251390f0b38cbcac8919b529c4c9d
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2017-06-18 19:58:00 +0200

    feat(fav): display list of favorite stations

--- velib-map/FavoriteTableViewController.swift
+++ velib-map/FavoriteTableViewController.swift
@@ -7,11 +7,34 @@
 //
 
 import UIKit
+import CoreStore
 
 class FavoriteTableViewController: UITableViewController {
+  
+  var favStations = [FavoriteStation]()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     self.title = "Liste des favoris"
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    self.favStations = CoreStore.fetchAll(From<FavoriteStation>()) ?? []
+    self.tableView.reloadData()
+  }
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.favStations.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "favoriteCell", for: indexPath)
+    cell.textLabel?.text = self.favStations[indexPath.row].name
+    return cell
   }
 }
