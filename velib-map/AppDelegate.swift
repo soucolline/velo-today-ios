fc5b659ccab99d5bfd070e3b855181082ce14ab7
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2017-06-27 15:47:43 +0200

    feat(develop): add splashscreen

--- velib-map/AppDelegate.swift
+++ velib-map/AppDelegate.swift
@@ -14,13 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white]
     UINavigationBar.appearance().tintColor = UIColor.white
     UINavigationBar.appearance().barTintColor = UIColor.orange
-    UIApplication.shared.statusBarStyle = .lightContent
+    UIApplication.shared.setStatusBarHidden(false, with: .none)
     
     let dataStack = DataStack(
       xcodeModelName: "velibMap",
@@ -35,7 +34,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     CoreStore.defaultStack = dataStack
     return true
   }
-
+  
   func applicationWillResignActive(_ application: UIApplication) {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
