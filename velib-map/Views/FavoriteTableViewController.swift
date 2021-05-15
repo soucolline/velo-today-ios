7a1e46e7ccf29229a292ad1d1e7dbb18910a8a97
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2021-05-15 23:01:42 +0200

    feat(combine): add pull to refresh to favorite tableview

--- velib-map/Views/FavoriteTableViewController.swift
+++ velib-map/Views/FavoriteTableViewController.swift
@@ -10,29 +10,45 @@ import UIKit
 import SVProgressHUD
 import Swinject
 
-class FavoriteTableViewController: UITableViewController {
-  
-  var loaderMessage = "Chargement de vos stations préférées"
+class FavoriteTableViewController: UIViewController {
+  @IBOutlet private var tableView: UITableView!
 
   private let presenter: FavoritePresenter = Assembler.inject(FavoritePresenter.self)
+  private let refreshControl = UIRefreshControl()
+  
+  var loaderMessage = "Chargement de vos stations préférées"
 
   override func viewDidLoad() {
     super.viewDidLoad()
     self.title = "Favoris"
     
     self.presenter.attach(self)
-    
-    self.tableView.tableFooterView = UIView(frame: .zero) // Hide empty cells
+
+    setupUI()
+    self.presenter.fetchFavoriteStations()
   }
   
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
-    self.presenter.fetchFavoriteStations()
+
   }
   
   override func viewWillDisappear(_ animated: Bool) {
     super.viewWillDisappear(animated)
     SVProgressHUD.dismiss()
+  }
+
+  private func setupUI() {
+    self.refreshControl.addTarget(self, action: #selector(reloadData), for: .valueChanged)
+
+    self.tableView.dataSource = self
+    self.tableView.delegate = self
+    self.tableView.refreshControl = self.refreshControl
+    self.tableView.tableFooterView = UIView(frame: .zero) // Hide empty cells
+  }
+
+  @objc private func reloadData() {
+    presenter.fetchFavoriteStations()
   }
   
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -49,48 +65,45 @@ class FavoriteTableViewController: UITableViewController {
 extension FavoriteTableViewController: FavoriteView {
   
   func onFetchStationsSuccess() {
-    DispatchQueue.main.async {
-      self.tableView.reloadData()
-    }
+    self.tableView.reloadData()
+    self.refreshControl.endRefreshing()
   }
   
   func onFetchStationsEmptyError() {
-    DispatchQueue.main.async {
-      self.tableView.reloadData()
-      SVProgressHUD.setMinimumDismissTimeInterval(100.0)
-      SVProgressHUD.showInfo(withStatus: "Vous n'avez pas encore de favoris")
-    }
+    self.tableView.reloadData()
+    self.refreshControl.endRefreshing()
+    SVProgressHUD.setMinimumDismissTimeInterval(100.0)
+    SVProgressHUD.showInfo(withStatus: "Vous n'avez pas encore de favoris")
   }
   
   func onFetchStationsError() {
-    DispatchQueue.main.async {
-      self.present(PopupManager.showErrorPopup(message: "Une erreur est survenue, veuillez réessayer"), animated: true)
-    }
+    self.refreshControl.endRefreshing()
+    self.present(PopupManager.showErrorPopup(message: "Une erreur est survenue, veuillez réessayer"), animated: true)
   }
   
 }
 
-extension FavoriteTableViewController {
+extension FavoriteTableViewController: UITableViewDelegate, UITableViewDataSource {
   
-  override func numberOfSections(in tableView: UITableView) -> Int {
+  func numberOfSections(in tableView: UITableView) -> Int {
     1
   }
   
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     self.presenter.getStationsCount()
   }
   
-  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
     100
   }
   
-  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     if let selectedStation = self.presenter.getStation(at: indexPath.row) {
       self.performSegue(withIdentifier: K.SegueIdentifiers.favoriteToDetailSegue, sender: selectedStation)
     }
   }
   
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: K.Identifiers.favoriteCell, for: indexPath) as? FavoriteTableViewCell
     
     if let station = self.presenter.getStation(at: indexPath.row) {
