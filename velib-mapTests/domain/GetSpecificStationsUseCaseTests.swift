e347b32cc715f3107b7bd69ad0902130b077cc4c
Thomas Guilleminot <thomas@Thomass-Mac-Studio.local>
2022-04-09 21:45:53 +0200

    feat(tests): add tests for domain layer

---
+++ velib-mapTests/domain/GetSpecificStationsUseCaseTests.swift
@@ -0,0 +1,51 @@
+//
+//  GetSpecificStationsUseCaseTests.swift
+//  velib-mapTests
+//
+//  Created by Thomas Guilleminot on 09/04/2022.
+//  Copyright © 2022 Thomas Guilleminot. All rights reserved.
+//
+
+import Foundation
+import Cuckoo
+import XCTest
+
+@testable import velib_map
+
+class GetSpecificStationsUseCaseTests: XCTestCase {
+    private let mockRepository = MockStationRepository()
+    private var useCase: GetSpecificStationsUseCase!
+    
+    override func setUp() {
+        useCase = GetSpecificStationsUseCase(stationRepository: mockRepository)
+    }
+    
+    override func tearDown() {
+        verifyNoMoreInteractions(mockRepository)
+    }
+    
+    func testInvoke() async throws {
+        let expectedDomainStation = [DomainStation(
+            freeDocks: 1,
+            code: "123",
+            name: "Test name",
+            totalDocks: 3,
+            freeBikes: 4,
+            freeMechanicalBikes: 5,
+            freeElectricBikes: 6,
+            geolocation: [12.23, 45.67]
+        )]
+        
+        let expectedUIStation = expectedDomainStation.map { $0.toUIITem() }
+        let expectedIds = ["1"]
+        
+        stub(mockRepository) { stub in
+            when(stub).fetchAllStations(from: any()).thenReturn(expectedDomainStation)
+        }
+        
+        let result = try await useCase.invoke(ids: expectedIds)
+        
+        XCTAssertEqual(result.first!.freeDocks, expectedUIStation.first!.freeDocks)
+        verify(mockRepository).fetchAllStations(from: expectedIds)
+    }
+}
