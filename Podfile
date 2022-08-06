3433c9d4242111fdefab88ff6388ec1654e1e6bb
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2022-08-06 21:08:19 +0200

    feat(tca): clean project settings for arm64

--- Podfile
+++ Podfile
@@ -31,7 +31,6 @@ post_install do |pi|
     pi.pods_project.targets.each do |t|
       t.build_configurations.each do |config|
         config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
-        config.build_settings["EXCLUDED_ARCHS[sdk=iphonesimulator*]"] = "arm64"
       end
     end
 end
---
+++ velib-map/TCA/SettingsView.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsView.swift
+//  velib-map
+//
+//  Created by Thomas Guilleminot on 06/08/2022.
+//  Copyright © 2022 Thomas Guilleminot. All rights reserved.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+struct SettingsViewTCA: View {
+    var body: some View {
+        Text("Settings view")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsViewTCA()
+    }
+}
